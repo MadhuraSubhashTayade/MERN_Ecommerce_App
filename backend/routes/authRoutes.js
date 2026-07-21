38a1510aef5175b9f918acd1c6f75af3656b9898
Madhura Tayade <tayademadhura02@gmail.com>
2026-07-22 00:15:39 +0530

    signup-route-implementation

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { signUpUser } from "../controllers/authController.js";
+const router = express.Router();
+
+router.post("/signup", signUpUser);
+export default router
